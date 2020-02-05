1645ce7289a088e7c1d91a354373ddee26bf036e
Tripp Martin <tripp.martin3@gmail.com>
2020-02-05 14:24:53 -0500

    changed rack version, added DB support

---
+++ db/schema.rb
@@ -0,0 +1,26 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_02_05_183022) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "user_info", force: :cascade do |t|
+    t.string "user_name"
+    t.string "chosen_letters", array: true
+    t.integer "guesses_remaining"
+    t.string "letters_left_to_guess", array: true
+    t.string "randomly_picked_word", array: true
+  end
+
+end
